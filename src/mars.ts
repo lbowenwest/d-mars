90f2a7634001c8e18c294d8b3b2dcd5a44397aff
Lygon Bowen-West <l.bowenwest@googlemail.com>
2023-10-12 14:33:58 +0100

    add prettier settings

--- src/mars.ts
+++ src/mars.ts
@@ -1,72 +1,72 @@
 import {
-  Direction,
-  Instruction,
-  InstructionSet,
-  Point,
-  Result,
-  World,
+    Direction,
+    Instruction,
+    InstructionSet,
+    Point,
+    Result,
+    World,
 } from "./types";
 
 export function rotate(start: Direction, instruction: Instruction): Direction {
-  if (instruction == "F") {
-    return start;
-  }
-  switch (start) {
-    case "N":
-      return instruction == "L" ? "W" : "E";
-    case "E":
-      return instruction == "L" ? "N" : "S";
-    case "S":
-      return instruction == "L" ? "E" : "W";
-    case "W":
-      return instruction == "L" ? "S" : "N";
-  }
+    if (instruction == "F") {
+        return start;
+    }
+    switch (start) {
+        case "N":
+            return instruction == "L" ? "W" : "E";
+        case "E":
+            return instruction == "L" ? "N" : "S";
+        case "S":
+            return instruction == "L" ? "E" : "W";
+        case "W":
+            return instruction == "L" ? "S" : "N";
+    }
 }
 
 export function advance(start: Point): [number, number] {
-  switch (start.direction) {
-    case "N":
-      return [start.position[0], start.position[1] + 1];
-    case "E":
-      return [start.position[0] + 1, start.position[1]];
-    case "S":
-      return [start.position[0], start.position[1] - 1];
-    case "W":
-      return [start.position[0] - 1, start.position[1]];
-  }
+    switch (start.direction) {
+        case "N":
+            return [start.position[0], start.position[1] + 1];
+        case "E":
+            return [start.position[0] + 1, start.position[1]];
+        case "S":
+            return [start.position[0], start.position[1] - 1];
+        case "W":
+            return [start.position[0] - 1, start.position[1]];
+    }
 }
 
 export function inBounds(
-  position: [number, number],
-  worldSize: [number, number],
+    position: [number, number],
+    worldSize: [number, number],
 ): boolean {
-  return !(
-    position[0] < 0 ||
-    position[0] > worldSize[0] ||
-    position[1] < 0 ||
-    position[1] > worldSize[1]
-  );
+    return !(
+        position[0] < 0 ||
+        position[0] > worldSize[0] ||
+        position[1] < 0 ||
+        position[1] > worldSize[1]
+    );
 }
 
 export function processInstruction(
-  world: World,
-  instructionSet: InstructionSet,
+    world: World,
+    instructionSet: InstructionSet,
 ): Result {
-  let point = instructionSet.start;
-  for (let instruction of instructionSet.instructions) {
-    point.direction = rotate(point.direction, instruction);
-    if (instruction == "F") {
-      const position = advance(point);
-      if (inBounds(position, world.size)) {
-        point.position = position;
-        continue;
-      }
-      if (world.lostPositions.has(point.position.join("-"))) {
-        continue;
-      }
-      world.lostPositions.add(point.position.join("-"));
-      return { point, lost: true };
+    let point = instructionSet.start;
+    for (let instruction of instructionSet.instructions) {
+        point.direction = rotate(point.direction, instruction);
+        if (instruction == "F") {
+            const position = advance(point);
+            if (inBounds(position, world.size)) {
+                point.position = position;
+                continue;
+            }
+            if (world.lostPositions.has(point.position.join("-"))) {
+                continue;
+            }
+            world.lostPositions.add(point.position.join("-"));
+            return { point, lost: true };
+        }
     }
-  }
-  return { point, lost: false };
+    return { point, lost: false };
 }
--- src/types.ts
+++ src/types.ts
@@ -1,18 +1,18 @@
 export type Direction = "N" | "E" | "S" | "W";
 export type Instruction = "L" | "R" | "F";
 export type World = {
-  size: [number, number];
-  lostPositions: Set<string>;
+    size: [number, number];
+    lostPositions: Set<string>;
 };
 export type Point = {
-  position: [number, number];
-  direction: Direction;
+    position: [number, number];
+    direction: Direction;
 };
 export type InstructionSet = {
-  start: Point;
-  instructions: Instruction[];
+    start: Point;
+    instructions: Instruction[];
 };
 export type Result = {
-  point: Point;
-  lost: boolean;
+    point: Point;
+    lost: boolean;
 };
